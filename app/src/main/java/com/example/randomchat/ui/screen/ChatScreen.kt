ff6e0d8d4accd8dc082547da45bbe3bb0c2807b8
Lavie <son.tran@ugotechs.com>
2025-06-15 14:47:53 +0700

    add string id instead of hard code text

--- app/src/main/java/com/example/randomchat/ui/screen/ChatScreen.kt
+++ app/src/main/java/com/example/randomchat/ui/screen/ChatScreen.kt
@@ -12,17 +12,24 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.res.stringResource
 import androidx.navigation.NavController
+import com.example.randomchat.R
 
 @Composable
 fun ChatScreen(
-    navController: NavController,
+    navController: NavController? = null,
     modifier: Modifier = Modifier,
     onStartChatClick: () -> Unit = {}
 ) {
@@ -40,17 +47,18 @@ fun ChatScreen(
         )
         Spacer(modifier = Modifier.height(24.dp))
         Text(
-            text = "Let’s start chatting!",
             fontSize = 22.sp,
             fontWeight = FontWeight.Medium,
-            color = Color.Black
+            color = Color.Black,
+            text = stringResource(R.string.lets_start_chatting)
+
         )
         Spacer(modifier = Modifier.height(32.dp))
         TextButton(
             onClick = onStartChatClick
         ) {
             Text(
-                text = "Start a chat",
+                text = stringResource(R.string.start_a_chat),
                 fontSize = 18.sp,
                 fontWeight = FontWeight.Bold,
                 color = Color(0xFF2979FF)
