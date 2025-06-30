f9a80ed61349d288d3b852a49ca9a2a6a7d9d738
Lavie <son.tran@ugotechs.com>
2025-06-30 18:06:01 +0700

    remove unused import

--- app/src/main/java/com/lavie/randochat/MainActivity.kt
+++ app/src/main/java/com/lavie/randochat/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.lavie.randochat
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
