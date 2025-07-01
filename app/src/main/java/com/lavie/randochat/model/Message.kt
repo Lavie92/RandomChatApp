ed027b053c747a8733517dd7d46acde47a57a815
Lavie <paintjinx1@gmail.com>
2025-07-01 14:46:58 +0700

    revert dont related code

--- app/src/main/java/com/lavie/randochat/model/Message.kt
+++ app/src/main/java/com/lavie/randochat/model/Message.kt
@@ -1,19 +1,9 @@
 package com.lavie.randochat.model
 
-enum class MessageType {
-    TEXT, IMAGE, AUDIO
-}
-
-enum class MessageStatus {
-    SENDING, SENT, RECEIVED, READ, FAILED
-}
-
 data class Message(
     val id: String = "",
     val senderId: String = "",
     val receiverId: String = "",
     val content: String = "",
-    val timestamp: Long = 0L,
-    val type: MessageType = MessageType.TEXT,
-    val status: MessageStatus = MessageStatus.SENT
+    val timestamp: Long = 0L
 )
